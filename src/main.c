6dad111d48db8c721a5fd40256310387038fe126
ShamsParvezArka <parvez6826@gmail.com>
2024-09-15 19:45:40 +0600

    refactored

--- src/main.c
+++ src/main.c
@@ -2,9 +2,12 @@
 #include <stdbool.h>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-
-#define GLUTIL_IMPLEMENTATION
+        
+#define CHFN_IMPLEMENTATION
 #include "glutil.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 int main()
 {
@@ -27,65 +30,78 @@ int main()
     
     glfwMakeContextCurrent(window);
 
-    gladInit();
-    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    chfnGladInit();
+    glfwSetFramebufferSizeCallback(window, chfnFramebufferSizeCallback);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 
-        -0.25f, 0.0f, 0.0f, 0.6f, 0.0f, 0.0f,
-        0.25f, 0.0f, 0.0f, 0.4f, 0.0f, 0.0f,
-        0.0f, -0.5f, 0.0f, 0.0, 0.0f, 1.0f
+    float vertices[] = 
+    {
+        // positions       // colors         //texture coords
+        0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+       -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // top left
     };
 
     unsigned int indices[] = 
-    {
-        0, 5, 3,
-        5, 1, 4,
-        3, 4, 2
+    { 
+        0, 1, 2,
+        0, 2, 3
     };
-    
-    
-    unsigned int shaderProgram = createShaderProgram("src/vshader.glsl", "src/fshader.glsl");
+
+    unsigned int shaderProgram = chfnCreateShaderProgram("src/vert.glsl", "src/frag.glsl");
     
     //Vertex buffer object, Vertex array object, Element buffer object
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    unsigned int VBO = chfnCreateVBO(sizeof(vertices), vertices);
+    unsigned int VAO = chfnCreateVAO();
 
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    // position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
-    glEnableVertexAttribArray(0);
-    // color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    unsigned int EBO = chfnCreateEBO(sizeof(indices), indices);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-    glfwSetKeyCallback(window, keyCallback);
+    unsigned int texture = 0;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int imageWidth, imageHeight, nrChannel;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *imageData = stbi_load("honeypie.png", &imageWidth, &imageHeight, &nrChannel, 0);
+
+    if (imageData)
+    {
+        //glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else
+    {
+        printf("error: failed to load texture\n");
+        return(EXIT_FAILURE);
+    }
+
+    stbi_image_free(imageData);
+
+    unsigned int textureUni = glGetUniformLocation(shaderProgram, "ourTexture");
+    chfnUseShaderProgram(shaderProgram);
+    glUniform1i(textureUni, 0);
+
+    glfwSetKeyCallback(window, chfnKeyCallback);
 
     while (!glfwWindowShouldClose(window)) 
     {
-        glClearColor(204.0f / 255.0f, 202.0f / 255.0f, 202.0f / 255.0f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        useShaderProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        chfnUseShaderProgram(shaderProgram);
 
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         //glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
